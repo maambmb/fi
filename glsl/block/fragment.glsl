30b42787c7a76805bf7a95fed79f548ea4b33be0
maambmb <timlonsdale@gmail.com>
2017-01-26 21:42:19 +0000

    finished fragment shader

---
+++ glsl/block/fragment.glsl
@@ -0,0 +1,19 @@
+#version 400 core;
+
+in vec2 frag_tex_coords;
+in vec3 frag_lighting;
+
+uniform sampler2D textureSampler;
+
+out vec4 out_Color;
+
+void main(void) {
+
+    // sample the texture to get the color
+    vec3 tex_color = texture( textureSampler, frag_tex_coords ).xyz;
+    // multiply the texture's color with the lighting to get the final pixel color
+    vec3 final_color = tex_color * frag_lighting;
+    // and boom we're done :-D
+    out_Color = vec4( final_color, 1.0 );
+
+}
--- glsl/block/vertex.glsl
+++ glsl/block/vertex.glsl
@@ -1,9 +1,9 @@
 #version 400 core;
 
 in vec3 av_position;
+in vec2 av_tex_coords;
 in int av_normal;
 in int av_shadow;
-in int av_block_type;
 in int av_lighting_global;
 in int av_lighting_constant;
 
@@ -16,11 +16,10 @@ uniform vec3 uv_fog;
 uniform vec3 uv_lighting_base;
 uniform vec3 uv_lighting_global;
 uniform vec3 uv_lighting_constant;
-uniform float uv_tex_coords[ 2 * 3 ];
 
-out vec3 out_lighting;
-out vec3 out_normal;
-out float out_shadow;
+out vec2 frag_tex_coords;
+out vec3 frag_lighting;
+out float frag_shadow;
 
 void main(void) {
 
@@ -28,17 +27,37 @@ void main(void) {
     ivec3 lighting_global_vec;
     float lighting_global_angle_factor;
 
-    gl_Position  = uv_projection_matrix * uv_view_matrix * uv_model_tsc_matrix * uv_model_rot_matrix * vec4(position,1.0);
-    out_normal   = vec3( av_normal & 0xFF, ( av_normal >> 8 ) & 0xFF, ( av_normal >> 16 ) & 0xFF );
-    out_shaodw   = av_shadow;
-    out_lighting = uv_lighting_ambient;
+    // maximum amount a shadow can reduce illumination
+    float shadow_attenuation = 0.8f; 
+    
+    // 3 is the max number for av_shadow by design
+    shadow_attenuation *= av_shadow / 3f; 
 
-    lighting_global_angle_factor = 0.5f + max( dot( normalize( uv_lighting_global_position ), out_normal ), 0f ) / 2f;
-    lighting_global_vec          = vec3( av_lighting_global & 0xFF, ( av_lighting_global >> 8 ) & 0xFF, ( av_lighting_global >> 16 ) & 0xFF ) / 255f;
-    out_lighting                 = max( out_lighting, max( lighting_global_vec, uv_lighting_global ) * lighting_global_angle_factor );
+    // calculate the normal vector by unpacking the bytes of av_normal
+    vec3 normal_vec = vec3( av_normal & 0xFF, ( av_normal >> 8 ) & 0xFF, ( av_normal >> 16 ) & 0xFF );
 
+    // get the final position by multiply the position by the model matrices -> view matrix -> proj matrix
+    gl_Position = uv_projection_matrix * uv_view_matrix * uv_model_tsc_matrix * uv_model_rot_matrix * vec4(position,1.0);
+    
+    // set final lighting to the base ambient amount
+    frag_lighting = uv_lighting_ambient;
+
+    // calculate the amount we should attenuate global lighting based on angle of the face (use normal_vec )
+    lighting_global_angle_factor = 0.5f + max( dot( normalize( uv_lighting_global_position ), normal)_vec  ), 0f ) / 2f;
+    // unpack the global lighting vector and convert into a float vec [0f-1f]
+    lighting_global_vec = vec3( av_lighting_global & 0xFF, ( av_lighting_global >> 8 ) & 0xFF, ( av_lighting_global >> 16 ) & 0xFF ) / 255f;
+    // join the *attenuated* global lighting with the current frag_lighting value using max
+    frag_lighting = max( frag_lighting, max( lighting_global_vec, uv_lighting_global ) * lighting_global_angle_factor );
+
+    // we now repeat the procedure with all other light sources (minus the angle attenuation)
     lighting_constant_vec = ivec3( av_lighting_constant & 0xFF, ( av_lighting_constant >> 8 ) & 0xFF, ( av_lighting_constant >> 16 ) & 0xFF ) / 255f;
-    out_lighting          = max( out_lighting, max( lighting_constant_vec, uv_lighting_constant ) );
+    frag_lighting = max( frag_lighting, max( lighting_constant_vec, uv_lighting_constant ) );
+
+    // once all light sources have been join in, attenuate the final signal with the shadow
+    frag_lighting *= ( 1 - shadow_attenuation );
+
+    // just pass the tex coords through *yawn*
+    frag_tex_coords = av_tex_coords;
 
 }
 
