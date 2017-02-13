37c3c377959d1d9a422475fd3195c11633241098
Tim Lonsdale <timlonsdale@Tims-MBP.lan>
2017-02-13 22:45:14 +0000

    hash fix

--- src/util/HashUtils.java
+++ src/util/HashUtils.java
@@ -3,13 +3,19 @@ package util;
 // utility object for computing hashcodes
 public class HashUtils {
 
-    public static int hash( Object... objs ) {
-    	int hash = 1;
-    	for( Object o : objs ) {
-    		hash += o.hashCode();
-    		hash *= 31;
-    	}
-    	return hash;
+    private static int accumulate( int old, Object o ) {
+    	old += o.hashCode();
+    	return old * 31;
+    }
+    
+    public static int hash( Object o1, Object o2 ) {
+    	int hash = accumulate( 1, o1 );
+    	return accumulate( hash, o2 );
+    }
+    
+    public static int hash( Object o1, Object o2, Object o3 ) {
+    	int hash = hash( o1, o2 );
+    	return accumulate( hash, o3 );
     }
     
 }
